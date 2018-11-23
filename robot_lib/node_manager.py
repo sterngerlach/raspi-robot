15bf10e36aa2ae6432a253fd8c6dc7fdae0e245a
sterngerlach <std.experimental.optional@gmail.com>
2018-11-24 03:15:58 +0900

    fix NodeManager class

--- robot_lib/node_manager.py
+++ robot_lib/node_manager.py
@@ -107,22 +107,16 @@ class NodeManager(object):
         self.__setup_spi(spi_channel=0, speed=MotorL6470.L6470_SPI_SPEED)
         self.__setup_spi(spi_channel=1, speed=MotorL6470.L6470_SPI_SPEED)
 
-        # モータの状態を保持するディクショナリ
-        self.__motor_state_dict = self.__process_manager.dict()
-        # モータへの命令を保持するキュー
-        self.__motor_command_queue = self.__process_manager.Queue()
         # 左右のモータを初期化
         self.__motor_left = MotorL6470(spi_channel=0)
         self.__motor_right = MotorL6470(spi_channel=1)
         # モータのノードを作成
         self.__motor_node = MotorNode(
-            self.__motor_state_dict, self.__msg_queue,
-            self.__motor_command_queue,
+            self.__process_manager, self.__msg_queue,
             self.__motor_left, self.__motor_right)
 
         # モータのノードを追加
-        self.__add_command_receiver_node(
-            "motor", self.__motor_state_dict, self.__motor_node)
+        self.__add_command_receiver_node("motor", self.__motor_node)
 
     def __setup_servo_motor_node(self, config_dict):
         """サーボモータのノードを初期化"""
@@ -130,10 +124,6 @@ class NodeManager(object):
         # GPIOを初期化
         self.__setup_gpio()
 
-        # サーボモータの状態を保持するディクショナリ
-        self.__servo_motor_dict = self.__process_manager.dict()
-        # サーボモータへの命令を保持するキュー
-        self.__servo_motor_command_queue = self.__process_manager.Queue()
         # サーボモータが使用するGPIOの端子
         self.__servo_motor_gpio_pin = 18
         # サーボモータのID
@@ -143,108 +133,101 @@ class NodeManager(object):
         self.__servo_motor = ServoGwsS03t(
             self.__servo_motor_gpio_pin, min_value=48, max_value=144,
             min_angle=0, max_angle=180, frequency=50)
+        # サーボモータのノードを作成
+        self.__servo_motor_node = ServoMotorNode(
+            self.__process_manager, self.__msg_queue,
+            self.__servo_id, self.__servo_motor)
 
         # サーボモータのノードを追加
         self.__add_command_receiver_node(
-            self.__servo_motor_dict, self.__msg_queue, 
-            self.__servo_motor_command_queue, self.__servo_id,
-            "servo" + str(self.__servo_id))
+            "servo" + str(self.__servo_id), self.__servo_motor_node)
 
     def __setup_srf02_node(self, config_dict):
         """超音波センサのノードを初期化"""
 
-        # 超音波センサの状態を保持するディクショナリ
-        self.__srf02_state_dict = self.__process_manager.dict()
         # 超音波センサを初期化
         self.__srf02 = Srf02()
         # 超音波センサのノードを作成
         self.__srf02_node = Srf02Node(
-            self.__srf02_state_dict, self.__msg_queue, self.__srf02,
+            self.__process_manager, self.__msg_queue, self.__srf02,
+            config_dict["near_obstacle_threshold"],
             config_dict["interval"], config_dict["addr_list"])
 
         # 超音波センサのノードを追加
-        self.__add_data_sender_node(
-            "srf02", self.__srf02_state_dict, self.__srf02_node)
+        self.__add_data_sender_node("srf02", self.__srf02_node)
 
     def __setup_julius_node(self, config_dict):
         """音声認識エンジンJuliusのノードを初期化"""
 
-        # 音声認識エンジンJuliusの状態を保持するディクショナリ
-        self.__julius_state_dict = self.__process_manager.dict()
-        
         # 音声認識エンジンJuliusを開始するスクリプトのパスを設定
         file_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
         script_path = file_dir.parent.joinpath("scripts", "julius-start.sh")
 
         # 音声認識エンジンJuliusのノードを初期化
         self.__julius_node = JuliusNode(
-            self.__julius_state_dict, self._msg_queue,
-            str(script_path))
+            self.__process_manager, self._msg_queue, str(script_path))
 
         # 音声認識エンジンJuliusのノードを追加
-        self.__add_data_sender_node(
-            "julius", self.__julius_state_dict, self.__julius_node)
+        self.__add_data_sender_node("julius", self.__julius_node)
 
     def __setup_openjtalk_node(self, config_dict):
         """音声合成システムOpenJTalkのノードを初期化"""
 
-        # 音声合成システムOpenJTalkの状態を保持するディクショナリ
-        self.__openjtalk_state_dict = self.__process_manager.dict()
-        # 音声合成システムOpenJTalkへの命令を保持するキュー
-        self.__openjtalk_command_queue = self.__process_manager.Queue()
-        
         # 音声合成システムOpenJTalkを開始するスクリプトのパスを設定
         file_dir = pathlib.Path(os.path.dirname(os.path.abspath(__file__))
         script_path = file_dir.parent.joinpath("scripts", "openjtalk-start.sh")
 
         # 音声合成システムOpenJTalkのノードを初期化
         self.__openjtalk_node = OpenJTalkNode(
-            self.__openjtalk_state_dict, self.__msg_queue,
-            self.__openjtalk_command_queue, str(script_path))
+            self.__process_manager, self.__msg_queue, str(script_path))
 
         # 音声合成システムOpenJTalkのノードを追加
-        self.__add_command_receiver_node(
-            "openjtalk", self.__openjtalk_state_dict, self.__openjtalk_node)
+        self.__add_command_receiver_node("openjtalk", self.__openjtalk_node)
 
     def __setup_webcam_node(self, config_dict):
         """ウェブカメラを操作するノードを初期化"""
 
-        # ウェブカメラの状態を保持するディクショナリ
-        self.__webcam_state_dict = self.__process_manager.dict()
         # ウェブカメラのノードを作成
         self.__webcam_node = WebCamNode(
-            self.__webcam_state_dict, self.__msg_queue, camera_id=0)
+            self.__process_manager, self.__msg_queue, camera_id=0)
         
         # ウェブカメラのノードを追加
-        self.__add_data_sender_node(
-            "webcam", self.__webcam_state_dict, self.__webcam_node)
+        self.__add_data_sender_node("webcam", self.__webcam_node)
 
-    def __add_data_sender_node(self, name, state_dict, node):
+    def __add_data_sender_node(self, name, node):
         """指定された名前を持つノードを追加"""
-        self.__data_sender_nodes[name] = { "states": state_dict, "node": node }
+        self.__data_sender_nodes[name] = node
         
-    def __add_command_receiver_node(self, name, state_dict, node):
+    def __add_command_receiver_node(self, name, node):
         """指定された名前を持つノードを追加"""
-        self.__command_receiver_nodes[name] = { "states": state_dict, "node": node }
+        self.__command_receiver_nodes[name] = node
 
     def run_nodes(self):
         """ノードの実行を開始"""
-        for data_sender in self.__data_sender_nodes:
-            data_sender["node"].run()
-
-        for command_receiver in self.__command_receiver_nodes:
-            command_receiver["node"].run()
+        for name, data_sender in self.__data_sender_nodes.items() :
+            data_sender.run()
+        for name, command_receiver in self.__command_receiver_nodes.items():
+            command_receiver.run()
     
     def send_command(self, name, cmd):
         """指定された名前のノードにコマンドを送信"""
-        self.__command_receiver_nodes[name]["node"].send_command(cmd)
+        self.__command_receiver_nodes[name].send_command(cmd)
+
+    def get_node(self, name):
+        """指定された名前のノードを取得"""
+        if name in self.__data_sender_nodes:
+            return self.__data_sender_nodes[name]
+        elif name in self.__command_receiver_nodes:
+            return self.__command_receiver_nodes[name]
+        else:
+            return None
 
     def get_node_state(self, name):
         """指定された名前のノードの状態を取得"""
         if name in self.__data_sender_nodes:
-            return self.__data_sender_nodes[name]["states"]
+            return self.__data_sender_nodes[name].state_dict
         elif name in self.__command_receiver_nodes:
-            return self.__command_receiver_nodes[name]["states"]
+            return self.__command_receiver_nodes[name].state_dict
         else:
             return None
 
