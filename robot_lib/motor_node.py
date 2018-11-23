c6139b2595d1a3e0fbfd0f09eb27f7233140a139
sterngerlach <std.experimental.optional@gmail.com>
2018-11-23 15:12:43 +0900

    rewrite MotorNode class

--- robot_lib/motor_node.py
+++ robot_lib/motor_node.py
@@ -1,117 +1,49 @@
 # coding: utf-8
-# motor_controller.py
+# motor_node.py
 
 import multiprocessing as mp
 import queue
 import time
 
-class MotorUnknownCommandException(Exception):
-    """
-    モータへ送信されたコマンドが未知であることを表す例外クラス
-    """
-    pass
+from command_receiver_node import CommandReceiverNode, UnknownCommandException
 
-class MotorController(object):
+class MotorNode(CommandReceiverNode):
     """
     ロボットのモータを操作するクラス
     """
     
-    def __init__(self, motor_left, motor_right,
-        command_queue, waiting_command_queue, info):
+    def __init__(self, state_dict, command_queue, motor_left, motor_right):
         """コンストラクタ"""
 
-        # モータへの命令を管理するキュー
-        self.command_queue = command_queue
-        # 実行待ちのモータの命令を管理するキュー
-        self.waiting_command_queue = waiting_command_queue
-        # モータの情報を管理するためのディクショナリ
-        self.info = info
-        self.info["speed_left"] = 0
-        self.info["speed_right"] = 0
-        self.info["is_command_executing"] = False
-
-        # モータへの命令を監視するためのプロセスを作成
-        # キューとディクショナリはいずれもプロセス間で共有されているので
-        # メソッドの引数に渡す必要がない
-        self.process_watcher = mp.Process(target=self.handle_command, args=())
-        # モータの命令を実行するためのプロセスを作成
-        self.process_executor = None
+        super().__init__(state_dict, command_queue)
+        
+        # モータの状態をディクショナリに格納
+        self.state_dict["speed_left"] = 0
+        self.state_dict["speed_right"] = 0
         
         # 左右のモータ
         self.motor_left = motor_left
         self.motor_right = motor_right
 
-        return
-
     def __del__(self):
         """デストラクタ"""
 
-        # プロセスを終了
-        # self.process_watcher.terminate()
+        # 2つのモータを停止
+        self.stop()
 
-        return
+        # 2つのモータの使用を終了
+        self.end()
 
-    def handle_command(self):
+    def process_command(self):
         """モータの命令を処理"""
         
         while True:
             # モータへの命令をキューから取り出し
             cmd = self.command_queue.get()
-            print("MotorController::handle_command(): " +
-                  "command received: {0}"
+            print("MotorNode::process_command(): command received: {0}"
                   .format(cmd))
-
-            # キャンセルのコマンドが送られた場合は現在のコマンドの実行を中止
-            if cmd["command"] == "cancel":
-                if self.process_executor is not None:
-                    # プロセスが存在する場合は強制終了
-                    print("MotorController::handle_command(): " +
-                          "terminating executor process")
-                    self.process_executor.terminate()
-                    self.process_executor = None
-
-                    # キャンセル命令が完了
-                    self.command_queue.task_done()
-                    
-                    # コマンドが実行途中であった場合にのみ完了したことを通知
-                    if self.info["is_command_executing"]:
-                        # 実行中であったコマンドが完了
-                        self.waiting_command_queue.task_done()
-
-                    continue
-            
+   
             # モータへの命令を実行
-            if self.process_executor is None:
-                # モータへの命令を実際に実行するためのプロセスを作成
-                self.process_executor = mp.Process(
-                    target=self.execute_command, args=())
-                # デーモンプロセスに設定
-                # 親プロセスが終了するときに子プロセスが自動的に終了
-                self.process_executor.daemon = True
-                self.process_executor.start()
-
-            # モータの命令キューに命令を追加
-            self.waiting_command_queue.put(cmd)
-            self.command_queue.task_done()
-
-        return
-
-    def execute_command(self):
-        """モータへの命令を実行"""
-
-        # コマンドは実行されていない
-        self.info["is_command_executing"] = False
-
-        while True:
-            # モータへの命令をキューから取り出し
-            cmd = self.waiting_command_queue.get()
-            print("MotorController::execute_command(): " +
-                  "command will be executed: {0}"
-                  .format(cmd))
-
-            # コマンドは実行されている
-            self.info["is_command_executing"] = True
-
             if cmd["command"] == "accel":
                 # 2つのモータを加速
                 self.accelerate(cmd["speed"], cmd["slope"])
@@ -143,48 +75,14 @@ class MotorController(object):
                 else:
                     self.rotate(cmd["direction"])
             else:
-                # 不明のコマンドを受信した場合は無視
-                print("MotorController::execute_command(): " +
-                      "unknown command ignored: {0}"
-                      .format(cmd["command"]))
-            
-            # コマンドは実行されていない
-            self.info["is_command_executing"] = False
+                # 不明のコマンドを受信した場合は例外をスロー
+                raise UnknownCommandException(
+                    "MotorNode::process_command(): unknown command: {0}"
+                    .format(cmd["command"]))
 
             # モータへの命令が完了
-            self.waiting_command_queue.task_done()
-            
-        return
-
-    def send_command(self, cmd):
-        """モータの命令キューに新たな命令を追加"""
-        self.command_queue.put(cmd)
-        return
-
-    def wait_until_all_command_done(self):
-        """モータへ送信した命令が全て実行されるまで待機"""
-        self.waiting_command_queue.join()
-        return
-
-    def run(self):
-        """モータの動作を開始"""
-
-        # プロセスを開始
-        self.process_watcher.start()
-
-        return
-
-    def emergency_stop(self):
-        """モータを緊急停止"""
-
-        # プロセスを終了
-        self.process_watcher.terminate()
-
-        # モータを停止
-        self.stop()
-
-        return
-
+            self.command_queue.task_done()
+    
     def accelerate(self, speed, wait_time):
         """2つのモータを加速"""
         
@@ -197,8 +95,6 @@ class MotorController(object):
 
             time.sleep(wait_time)
 
-        return
-
     def accelerate_left(self, speed, wait_time):
         """左のモータを加速"""
         
@@ -207,8 +103,6 @@ class MotorController(object):
             self.motor_left.run(self.info["speed_left"])
             time.sleep(wait_time)
 
-        return
-
     def accelerate_right(self, speed, wait_time):
         """右のモータを加速"""
         
@@ -216,8 +110,6 @@ class MotorController(object):
             self.info["speed_right"] += 250
             self.motor_right.run(-self.info["speed_right"])
             time.sleep(wait_time)
-
-        return
 
     def decelerate(self, speed, wait_time):
         """2つのモータを減速"""
@@ -232,8 +124,6 @@ class MotorController(object):
 
             time.sleep(wait_time)
 
-        return
-
     def decelerate_left(self, speed, wait_time):
         """左のモータを減速"""
 
@@ -241,8 +131,6 @@ class MotorController(object):
             self.info["speed_left"] -= 250
             self.motor_left.run(self.info["speed_left"])
             time.sleep(wait_time)
-
-        return
     
     def decelerate_right(self, speed, wait_time):
         """右のモータを減速"""
@@ -252,9 +140,6 @@ class MotorController(object):
             self.motor_right.run(-self.info["speed_right"])
             time.sleep(wait_time)
 
-        return
-
-
     def stop(self):
         """2つのモータを停止"""
 
@@ -263,8 +148,6 @@ class MotorController(object):
         
         self.motor_left.run(self.info["speed_left"])
         self.motor_right.run(self.info["speed_right"])
-        
-        return
 
     def end(self):
         """2つのモータの使用を終了"""
@@ -276,8 +159,6 @@ class MotorController(object):
         # モータのブリッジを高インピーダンスに設定
         self.motor_left.softhiz()
         self.motor_right.softhiz()
-        
-        return
 
     def rotate(self, direction, wait_time=1.5):
         if direction == "left":
@@ -310,8 +191,6 @@ class MotorController(object):
         # 回転後は停止
         self.stop()
 
-        return
-
     def rotate_right(self, wait_time=1.5):
         """ロボットを回転"""
         
@@ -333,6 +212,4 @@ class MotorController(object):
 
         # 回転後は停止
         self.stop()
-
-        return
 
