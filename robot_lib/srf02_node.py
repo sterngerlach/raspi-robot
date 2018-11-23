7d88adf01c6b3160f32ea98b9174c1678927377c
sterngerlach <std.experimental.optional@gmail.com>
2018-11-23 15:13:40 +0900

    rewrite Srf02Node class

--- robot_lib/srf02_node.py
+++ robot_lib/srf02_node.py
@@ -1,74 +1,50 @@
 # coding: utf-8
-# srf02_controller.py
+# srf02_node.py
 
 import multiprocessing as mp
 import time
 
-class Srf02Controller(object):
+from data_sender_node import DataSenderNode
+
+class Srf02Node(DataSenderNode):
     """
-    超音波センサ(SRF02)を操作するクラス
+    超音波センサ(Srf02)を操作するクラス
     """
 
-    def __init__(self, srf02, motor_command_queue):
+    def __init__(self, state_dict, srf02, interval=0.5, addr_list=[0x70]):
         """コンストラクタ"""
 
-        # モータへの命令を管理するキュー
-        self.motor_command_queue = motor_command_queue
-        # 超音波センサの入力を処理するためのプロセスを作成
-        self.process_handler = mp.Process(
-            target=self.process_input, args=(self.motor_command_queue,))
-        # デーモンプロセスに設定
-        # 親プロセスが終了するときに子プロセスを終了させる
-        self.process_handler.daemon = True
+        super().__init__(state_dict)
 
-        # 超音波センサ(SRF02)
+        # 超音波センサ(Srf02)
         self.srf02 = srf02
+        # 超音波センサの値を取得する間隔
+        self.interval = interval
+        # 超音波センサのアドレスのリスト
+        self.addr_list = addr_list
 
-        return
+        # 各アドレスに対応する超音波センサの情報を初期化
+        for addr in self.addr_list:
+            self.state_dict[addr] = None
+            
+    def update(self):
+        """入力を処理して状態を更新"""
 
-    def __del__(self):
-        """デストラクタ"""
+        try:
+            while True:
+                for addr in self.addr_list:
+                    # 各アドレスの超音波センサから距離を取得
+                    result = self.srf02.get_values(addr)
+
+                    if result is not None:
+                        # 測距データと最小の距離を取得
+                        dist, mindist = result
+                        # 各アドレスの超音波センサの情報を更新
+                        self.state_dict[addr] = { "dist": dist, "mindist": mindist }
+
+                time.sleep(self.interval)
+
+        except KeyboardInterrupt:
+            # プロセスが割り込まれた場合
+            print("Srf02Node::update(): KeyboardInterrupt occurred")
         
-        # プロセスを終了
-        # self.process_handler.terminate()
-
-        return
-
-    def process_input(self, motor_command_queue):
-        """超音波センサの入力を処理"""
-
-        while True:
-            # 距離の取得(アドレス0xE0のセンサ)
-            result = self.srf02.get_values(0x70)
-
-            if result is not None:
-                # 測距データと最小の距離を取得
-                dist, mindist = result
-
-                # print("Srf02Controller::process_input(): " +
-                #       "address: {0}, dist: {1} cm, mindist: {2} cm"
-                #       .format(0xE0, dist, mindist))
-
-                if dist < 15:
-                    print("Srf02Controller::process_input(): " +
-                          "motor will be stopped, since obstacle has been detected")
-                    motor_command_queue.put({ "command": "stop" })
-
-            time.sleep(0.5)
-
-    def run(self):
-        """超音波センサの利用を開始"""
-
-        # プロセスを開始
-        self.process_handler.start()
-
-        return
-
-    def emergency_stop(self):
-        """超音波センサの利用を緊急停止"""
-
-        # プロセスを終了
-        self.process_handler.terminate()
-
-        return
-
