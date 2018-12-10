c03cb0c1a6436f68752a942f4d949906ee51446c
sterngerlach <std.experimental.optional@gmail.com>
2018-12-10 16:50:41 +0900

    implement card detection node and server

---
+++ card_detection_server.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# card_detection_server.py
+
+import cv2
+import os
+import pickle
+import socket
+import struct
+import time
+import zlib
+
+def process_image(sock, addr):
+    # 画像のサイズを受信
+    msg_size = struct.calcsize("!i")
+
+    # 画像の横幅を受信
+    recv_data = sock.recv(msg_size)
+    frame_width = struct.unpack("!i", recv_data)[0]
+    send_data = struct.pack("!i", 123)
+    sock.sendall(send_data)
+    print("process_image(): frame width: {0}".format(frame_width))
+
+    # 画像の縦幅を受信
+    recv_data = sock.recv(msg_size)
+    frame_height = struct.unpack("!i", recv_data)[0]
+    send_data = struct.pack("!i", 456)
+    sock.sendall(send_data)
+    print("process_image(): frame height: {0}".format(frame_height))
+
+    while True:
+        # 画像のサイズを受信
+        msg_header_size = struct.calcsize("!L")
+        recv_data = sock.recv(msg_header_size)
+        frame_size = struct.unpack("!L", recv_data)
+        print("process_image(): frame size: {0}".format(frame_size))
+
+        # 画像を受信
+        frame_data = sock.recv(frame_size)
+        print("process_image(): image received (shape: {0})".format(frame_data.shape))
+
+        # 画像を検出
+        
+        # 検出されたカードの枚数を送信
+        cards_num = 2
+        sock.sendall(struct.pack("!i", cards_num))
+        recv_data = sock.recv(msg_size)
+        recv_data = struct.unpack("!i", recv_data)[0]
+        print("process_image(): magic value received: {0}".format(recv_data))
+
+        # 検出されたカードの数字を送信
+        for i in range(cards_num):
+            sock.sendall(struct.pack("!i", i))
+            recv_data = sock.recv(msg_size)
+            recv_data = struct.unpack("!i", recv_data)[0]
+            print("process_image(): magic value received: {0}".format(recv_data))
+
+def main():
+    host = sys.argv[1]
+    port = 12345
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    sock.bind((host, port))
+    sock.listen(5)
+
+    while True:
+        conn, addr = sock.accept()
+        print("main(): client connected: address: {0}, client: {1}"
+              .format(conn, addr))
+
+        process_image(conn, addr)
+
+        cv2.destroyAllWindows()
+        conn.close()
+
+if __name__ == "__main__":
+    main()
+
---
+++ robot_lib/card_detection_node.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+# card_detection_node.py
+
+import cv2
+import multiprocessing as mp
+import os
+import pathlib
+import pickle
+import socket
+import struct
+import subprocess as sp
+import time
+import zlib
+
+from command_receiver_node import CommandReceiverNode, UnknownCommandException
+
+class CardDetectionNode(CommandReceiverNode):
+    """
+    トランプのカードを検出するクラス
+    """
+
+    # 検出サーバとの通信で使用するポート番号
+    SERVER_PORT = 12345
+    
+    def __init__(self, process_manager, msg_queue, server_host,
+                 camera_id, frame_width, frame_height):
+        """コンストラクタ"""
+        super().__init__(process_manager, msg_queue)
+
+        # 検出サーバのIPアドレスまたはホスト名
+        self.server_host = server_host
+        # TCPソケットを作成
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # TIME_WAIT状態のポートをbindできるように設定
+        self.client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+
+        # 検出サーバに接続
+        self.client_socket.connect((self.server_host, CardDetectionNode.SERVER_PORT))
+
+        print("CardDetectionNode::__init__(): " +
+              "connected to card detection server (host: {0}, port: {1})"
+              .format(self.server_host, CardDetectionNode.SERVER_PORT))
+
+        # ビデオ撮影デバイスの作成
+        self.camera_id = camera_id
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        
+        # ビデオ撮影デバイスのパラメータの設定
+        self.video_capture = cv2.VideoCapture(camera_id)
+        self.video_capture.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        self.video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+        # キャプチャする画像のサイズを送信
+        msg_size = struct.calcsize("!i")
+        
+        # キャプチャする画像の横幅を送信
+        send_data = struct.pack("!i", self.frame_width)
+        self.client_socket.sendall(send_data)
+        recv_data = self.recv(msg_size)
+        recv_data = struct.unpack("!i", recv_data)[0]
+        print("CardDetectionNode::__init__(): magic value received: {0}".format(recv_data))
+        
+        # キャプチャする画像の縦幅を送信
+        send_data = struct.pack("!i", self.frame_height)
+        self.client_socket.sendall(send_data)
+        self.recv(msg_size)
+        recv_data = self.recv(msg_size)
+        recv_data = struct.unpack("!i", recv_data)[0]
+        print("CardDetectionNode::__init__(): magic value received: {0}".format(recv_data))
+
+    def __del__(self):
+        """デストラクタ"""
+
+        # ビデオ撮影デバイスの解放
+        self.video_capture.release()
+
+    def process_command(self):
+        """カードの検出命令を処理"""
+
+        try:
+            while True:
+                # カードの検出命令をキューから取り出し
+                cmd = self.command_queue.get()
+                print("CardDetectionNode::process_command(): command received: {0}".format(cmd))
+
+                # 命令でない場合は例外をスロー
+                if "command" not in cmd:
+                    raise UnknownCommandException(
+                        "CardDetectionNode::process_command(): unknown command: {0}"
+                        .format(cmd))
+                
+                # 命令の実行開始をアプリケーションに伝達
+                self.send_message("card", { "cmd": "detect", "state": "start" })
+                
+                if cmd["command"] == "detect":
+                    # カードの検出を実行
+                    cards = self.detect()
+
+                    print("CardDetectionNode::process_command(): number of cards detected: {0}"
+                          .format(len(cards)))
+
+                    # 検出結果をアプリケーションに伝達
+                    self.send_message("card",
+                        { "command": cmd["command"], "state": "detected", "cards": cards })
+                else:
+                    # 解釈できない命令の無視をアプリケーションに伝達
+                    self.send_message("card", { "command": cmd["command"], "state": "ignored" })
+                
+                # 命令の実行を完了
+                self.command_queue.task_done()
+        
+        except KeyboardInterrupt:
+            # プロセスが割り込まれた場合
+            print("CardDetectionNode::process_command(): KeyboardInterrupt occurred")
+    
+    def detect(self):
+        """カードの検出"""
+
+        # 画像データを作成
+        ret, frame = self.video_capture.read()
+        frame_data = pickle.dumps(frame)
+        frame_data = zlib.compress(frame_data)
+        frame_size = len(frame_data)
+        
+        # 画像データを送信
+        self.client_socket.sendall(struct.pack("!L", frame_size) + frame_data)
+
+        # 検出されたカードの枚数を取得
+        msg_size = struct.calcsize("!i")
+        recv_data = self.client_socket.recv(msg_size)
+        cards_num = struct.unpack("!i", recv_data)[0]
+        self.client_socket.sendall(struct.pack("!i", 789))
+
+        # 検出されたカードの数字を取得
+        cards = []
+
+        for i in range(cards_num):
+            recv_data = self.client_socket.recv(msg_size)
+            card_num = struct.unpack("!i", recv_data)[0]
+            cards.append(card_num)
+            self.client_socket.sendall(struct.pack("!i", 789))
+
+        return cards
+
