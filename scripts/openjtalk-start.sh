7c97a421425ee16c0543ce4bdab15534b7f933a2
sterngerlach <std.experimental.optional@gmail.com>
2018-12-19 13:47:50 +0900

    use female voice

--- scripts/openjtalk-start.sh
+++ scripts/openjtalk-start.sh
@@ -6,10 +6,12 @@
 # 保存先のファイル名を設定
 OUT_FILE=/tmp/out.wav
 
+# -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
+
 # コマンドライン引数に指定した日本語の文字列を音声ファイルに変換
 # 作成した音声ファイルを再生して削除
 echo "$1" | open_jtalk \
--m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
+-m /usr/share/hts-voice/mei/mei_happy.htsvoice \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
 -ow $OUT_FILE && \
 aplay --quiet $OUT_FILE
