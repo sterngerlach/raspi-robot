c648b3283e9887a00dd0238ed808c8e5e9841095
sterngerlach <std.experimental.optional@gmail.com>
2018-12-19 11:08:22 +0900

    tiny fix

--- card_ssd_dummy.py
+++ card_ssd_dummy.py
@@ -9,11 +9,14 @@ def card_detect(image):
     """ダミーのトランプカード検出関数"""
     print("card_detect(): image.shape: {}".format(image.shape))
     
-    card_num = random.randint(0, 3)
+    card_num = random.randint(0, 2)
     cards = []
 
     for i in range(card_num):
         cards.append(random.randint(1, 13))
+
+    print("card_detect(): number of cards detected: {}".format(card_num))
+    print("card_detect(): detected cards: {}".format(cards))
 
     return cards
 
