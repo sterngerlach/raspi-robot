02a20a2ca5456f4738b7186fd167e73c62e16e66
sterngerlach <std.experimental.optional@gmail.com>
2018-12-21 17:24:11 +0900

    fix audio-generate.sh to generate female voice

--- scripts/audio-generate.sh
+++ scripts/audio-generate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# -m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
 echo "$1" | open_jtalk \
--m /usr/share/hts-voice/nitech-jp-atr503-m001/nitech_jp_atr503_m001.htsvoice \
+-m /usr/share/hts-voice/mei/mei_happy.htsvoice \
 -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
 -ow "$2"
 
